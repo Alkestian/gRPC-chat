bb12fe35f684bc8f9b3a7d0fc5ed44ffe17eb13e
Alkestian <cameronblack51@hotmail.co.uk>
2025-02-19 22:26:02 +0000

    Adding imports

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net"
+
+	"github.com/Alkestian/grpc-chat/chat"
+	"google.golang.org/grpc"
 )
 
 func main() {
@@ -11,5 +14,13 @@ func main() {
 		log.Fatal("error starting http server: %s", err)
 	}
 	
-	
+	s := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatal("error starting grpc server: %s", err)
+	}
 }
